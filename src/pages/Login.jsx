a4346b1c75a7ca7a09b8ec8f458a7b563bd4442b
Fazliddin Usmonov <usmonovfazliddin2004>
2026-04-01 19:04:18 +0500

    fix: case-sensitive css filename

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { supabase } from "../supabase";
-import "../styles/Login.css";
+import "../styles/login.css";
 
 export default function Login() {
   const navigate = useNavigate();
